c6ac7db1fb21412222284d5757cf222aeffc9175
Prajapati Harsh <147126126+Prajapati-Harsh-2005@users.noreply.github.com>
2026-03-13 16:22:36 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App(){
+  return(
+  <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </Router>
+  </div>
+  )
+}
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+    return(
+        <>
+        <div id="footer">
+	<p>2025. Untitled. All rights reserved. Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>. Photos by <a href="http://fotogrph.com/">fotogrph</a>.</p>
+</div>
+        </>
+    )
+}
+export default Footer;
